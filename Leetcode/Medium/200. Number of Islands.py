0de2550e127aa7a1db227f52f44340b8f1194026
Raunaq S. <raunaqsingh328@gmail.com>
2026-02-11 23:11:24 +0000

    added Leetcode medium challenges

--- Leetcode/Medium/200. Number of Islands.py
+++ Leetcode/Medium/200. Number of Islands.py
@@ -75,4 +75,158 @@ if __name__ == "__main__":
 # Aux space, excluding output and input: O(k), k = number of ones in the grid
 # Total space, including output, excluding input: O(k)
 
+# Learning lessons (done after completing all of above in 1h 12 mins):
+#   - I now realise my solution can be improved, my rewrite is below:
+#
+# def numIslands(self, grid: List[List[str]]) -> int:
+#     # Time: O(N), N = number of points in grid
+#     # Aux space, excluding output and input: O(k), k = number of "1"'s in
+#     # grid, worst case O(N)
+#     # Total space, including output, excluding input: O(k), worst case O(N)
+#     total = 0 
+#     visited = set()
+#     q = deque()
+#     for y in range(len(grid)):
+#         for x in range(len(grid[0])):
+#             if (y, x) not in visited and grid[y][x] == "1":
+#                 q.append((y, x))
+#                 visited.add((y, x))
+#                 while q:
+#                     j, i = q.popleft()
+#                     for dj, di in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
+#                         new_j = j + dj
+#                         new_i = i + di
+#                         if 0 <= new_j < len(grid) and 0 <= new_i < len(grid[0]) and grid[new_j][new_i] == "1" and (new_j, new_i) not in visited:
+#                             q.append((new_j, new_i))
+#                             visited.add((new_j, new_i))
+#                 total += 1
+#     return total
+#
+#   - Additionally, there is also a depth-first-search version of the above,
+#     my attempt is below:
+#
+# def numIslands(self, grid: List[List[str]]) -> int:
+#     # Time: O(N), N = number of points in grid
+#     # Aux space, excluding output and input: O(k), k = number of 1's in grid,
+#     # worst case O(N) if grid consists of only 1's
+#     # Total space, including output, excluding input: O(k), worst case O(N)
+#     total = 0
+#     visited = set()
+#     stack = []
+#     for y in range(len(grid)):
+#         for x in range(len(grid[0])):
+#             if (y, x) not in visited and grid[y][x] == "1":
+#                 stack.append((y, x))
+#                 visited.add((y, x))
+#                 while stack:
+#                     j, i = stack.pop()
+#                     for dj, di in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+#                         new_j = j + dj
+#                         new_i = i + di
+#                         if 0 <= new_j < len(grid) and 0 <= new_i < len(grid[0]) and (new_j, new_i) not in visited and grid[new_j][new_i] == "1":
+#                             stack.append((new_j, new_i))
+#                             visited.add((new_j, new_i))
+#                 total += 1
+#     return total
+#
+#   - Additionally, there is also an iterative breadth-first-search method
+#     without using a set by changing 1's to 0's. My attempt is below:  
+#
+# def numIslands(self, grid: List[List[str]]) -> int:
+#     # Time: O(N), N = number of points in grid
+#     # Aux space, excluding output and input: O(Q), Q = max size reached by queue,
+#     # worst case O(N) if grid only consists of 1's
+#     # Total space, including output, excluding input: O(Q), worst case O(N)
+#     total = 0
+#     q = deque()
+#     for y in range(len(grid)):
+#         for x in range(len(grid[0])):
+#             if grid[y][x] == "1":
+#                 q.append((y, x))
+#                 grid[y][x] = "0"          
+#                 while q:
+#                     j, i = q.popleft()
+#                     for dj, di in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+#                         new_j = j + dj
+#                         new_i = i + di
+#                         if 0 <= new_j < len(grid) and 0 <= new_i < len(grid[0]) and grid[new_j][new_i] == "1":
+#                             q.append((new_j, new_i))
+#                             grid[new_j][new_i] = "0"
+#                 total += 1    
+#     return total
+#
+#   - Additionally, there is also an iterative depth-first-search method
+#     without using a set by changing 1's to 0's. My attempt is below:
+#
+# def numIslands(self, grid: List[List[str]]) -> int:
+#     # Time: O(N), N = number of points in grid
+#     # Aux space, excluding output and input: O(k), k = max size reached by
+#     # stack, worst case O(N) if grid only consists of 1's 
+#     # Total space, including output, excluding and input: O(k), worst case O(N) 
+#     stack = []
+#     total = 0
+#     for y in range(len(grid)):
+#         for x in range(len(grid[0])):
+#             if grid[y][x] == "1":
+#                 stack.append((y, x))
+#                 grid[y][x] = "0"
+#                 while stack:
+#                     j, i = stack.pop()       
+#                     for dj, di in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+#                         new_j = j + dj
+#                         new_i = i + di
+#                         if 0 <= new_j < len(grid) and 0 <= new_i < len(grid[0]) and grid[new_j][new_i] == "1":
+#                             stack.append((new_j, new_i))
+#                             grid[new_j][new_i] = "0"
+#                 total += 1
+#     return total
+#
+#   - Additionally, there is also a recursive depth-first-search method
+#     without using a set by changing 1's to 0's. My attempt is below:
+#
+# def numIslands(self, grid: List[List[str]]) -> int:
+#     # Time: O(N), N = number of points in the grid
+#     # Aux space, excluding output and input: worst case O(N) due to recursion
+#     # stack if grid only consists of 1's
+#     # Total space, including output, excluding input: worst case O(N)
+#     total = 0
+#     def dfs(j: int, i: int) -> None:
+#         if 0 <= j < len(grid) and 0 <= i < len(grid[0]) and grid[j][i] == "1":
+#             grid[j][i] = "0"
+#             for dj, di in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+#                 new_j = j + dj
+#                 new_i = i + di
+#                 dfs(new_j, new_i)
+#     for y in range(len(grid)):
+#         for x in range(len(grid[0])):
+#             if grid[y][x] == "1":
+#                 dfs(y, x)
+#                 total += 1
+#     return total
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
